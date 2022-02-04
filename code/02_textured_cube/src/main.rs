582d3554941b5c612e9fbc7cc6d2c8b699ba0f48
Luca Quartesan <quarte1994@gmail.com>
2022-02-04 01:04:27 +0100

    barycentric coordinates

--- code/02_textured_cube/src/main.rs
+++ code/02_textured_cube/src/main.rs
@@ -1,5 +1,4 @@
-//with extern we can make it availble everywhere
-use glam::Vec2;
+use glam::{Vec2, Vec3};
 use minifb::{Key, Window, WindowOptions};
 
 const WIDTH: usize = 500;
@@ -8,6 +7,15 @@ const HEIGHT: usize = 500;
 //accessible from other modules
 pub mod utils;
 pub use utils::*;
+
+pub fn barycentric_coordinates(point: Vec2, v0: Vec2, v1: Vec2, v2: Vec2, area: f32) -> Vec3 {
+    let m0 = edge_function(point, v1, v2);
+    let m1 = edge_function(point, v2, v0);
+    let m2 = edge_function(point, v0, v1);
+    // instead of 3 divisions we can do 1/area *
+    let a = 1.0 / area;
+    glam::vec3(m0 * a, m1 * a, m2 * a)
+}
 
 fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
@@ -37,16 +45,18 @@ fn main() {
         let coords = index_to_coords(i, WIDTH);
         // shadowing a variable
         let coords = glam::vec2(coords.0 as f32, coords.1 as f32);
-        let m2 = edge_function(coords, triangle[0], triangle[1]);
 
-        let m0 = edge_function(coords, triangle[1], triangle[2]);
-        let m1 = edge_function(coords, triangle[2], triangle[0]);
-        // if m0 & m1 & m2 >= 0 we are inside the triangle
-        if m0 >= 0.0 && m1 >= 0.0 && m2 >= 0.0 {
-            *pixel = to_argb8(255, (m2 * 255.0) as u8, 0, 0);
-        } else {
-            *pixel = 0;
-        }
+        // Triangle area
+        let area = edge_function(triangle[0], triangle[1], triangle[2]);
+        // subtriangles area / triangle area
+        let bary = barycentric_coordinates(coords, triangle[0], triangle[1], triangle[2], area);
+
+        *pixel = to_argb8(
+            255,
+            (bary.x * 255.0) as u8,
+            (bary.y * 255.0) as u8,
+            (bary.z * 255.0) as u8,
+        );
     }
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
