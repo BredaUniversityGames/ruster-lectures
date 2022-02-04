baf95a3587732fb88777153a2cb98dbb229bd3d4
Luca Quartesan <quarte1994@gmail.com>
2022-02-04 01:23:12 +0100

    raster_triangle function

--- code/02_textured_cube/src/main.rs
+++ code/02_textured_cube/src/main.rs
@@ -27,9 +27,32 @@ pub fn barycentric_coordinates(
     }
 }
 
+#[derive(Debug, Copy, Clone)]
 pub struct Vertex {
     pub position: Vec2,
     pub color: Vec3,
+}
+
+pub fn raster_triangle(v0: Vertex, v1: Vertex, v2: Vertex, buffer: &mut Vec<u32>){
+    for (i, pixel) in buffer.iter_mut().enumerate() {
+        let coords = index_to_coords(i, HEIGHT);
+        // shadowing a variable
+        let coords = glam::vec2(coords.0 as f32, coords.1 as f32);
+
+        let area = edge_function(v0.position, v1.position, v2.position);
+
+        if let Some(bary) =
+        barycentric_coordinates(coords, v0.position, v1.position, v2.position, area)
+        {
+            let color = bary.x * v0.color + bary.y * v1.color + bary.z * v2.color;
+            *pixel = to_argb8(
+                255,
+                (color.x * 255.0) as u8,
+                (color.y * 255.0) as u8,
+                (color.z * 255.0) as u8,
+            );
+        }
+    }
 }
 
 fn main() {
@@ -61,27 +84,7 @@ fn main() {
     // Limit to max ~60 fps update rate
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
-    for (i, pixel) in buffer.iter_mut().enumerate() {
-        let coords = index_to_coords(i, WIDTH);
-        let coords = glam::vec2(coords.0 as f32, coords.1 as f32);
-
-        // Triangle area
-        let area = edge_function(v0.position, v1.position, v2.position);
-
-        if let Some(bary) =
-            barycentric_coordinates(coords, v0.position, v1.position, v2.position, area)
-        {
-            let color = bary.x * v0.color + bary.y * v1.color + bary.z * v2.color;
-            *pixel = to_argb8(
-                255,
-                (color.x * 255.0) as u8,
-                (color.y * 255.0) as u8,
-                (color.z * 255.0) as u8,
-            );
-        } else {
-            *pixel = 0;
-        }
-    }
+    raster_triangle(v0, v1, v2, &mut buffer);
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
