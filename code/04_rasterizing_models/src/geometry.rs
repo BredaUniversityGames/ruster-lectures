fdbb95009b90791e68ea923059d8e2cfac12ad0a
Luca Quartesan <quarte1994@gmail.com>
2022-02-08 16:56:40 +0100

    raster clipping, engine goes brrr 🏎️

--- code/04_rasterizing_models/src/geometry.rs
+++ code/04_rasterizing_models/src/geometry.rs
@@ -126,3 +126,24 @@ impl AddAssign for Mesh {
         self.add_section_from_vertices(rhs.triangles(), rhs.vertices());
     }
 }
+
+pub struct BoundingBox2D {
+    pub left: f32,
+    pub right: f32,
+    pub top: f32,
+    pub bottom: f32,
+}
+
+pub fn get_triangle_bounding_box_2d(positions: &[Vec2; 3]) -> BoundingBox2D {
+    let left = positions[0].x.min(positions[1].x).min(positions[2].x);
+    let right = positions[0].x.max(positions[1].x).max(positions[2].x);
+    let top = positions[0].y.min(positions[1].y).min(positions[2].y);
+    let bottom = positions[0].y.max(positions[1].y).max(positions[2].y);
+
+    BoundingBox2D {
+        left,
+        right,
+        top,
+        bottom,
+    }
+}
--- code/04_rasterizing_models/src/lib.rs
+++ code/04_rasterizing_models/src/lib.rs
@@ -7,7 +7,7 @@ pub mod transform;
 pub mod utils;
 pub use {
     camera::Camera,
-    geometry::{Mesh, Vertex},
+    geometry::*,
     texture::Texture,
     transform::{Transform, TransformInitialParams},
     utils::*,
@@ -78,35 +78,35 @@ pub fn raster_triangle(
         map_to_range(-ndc2.y, -1.0, 1.0, 0.0, viewport_size.y),
     );
 
-    for (i, pixel) in buffer.iter_mut().enumerate() {
-        let coords = index_to_coords(i, viewport_size.y as usize);
-        // center of the pixel
-        let coords = glam::vec2(coords.0 as f32, coords.1 as f32) + 0.5;
+    if let Some(bb) = triangle_screen_bounding_box(&[sc0, sc1, sc2], viewport_size) {
+        for y in (bb.top as usize)..=bb.bottom as usize {
+            for x in (bb.left as usize)..=bb.right as usize {
+                let coords = glam::vec2(x as f32, y as f32) + 0.5;
+                let pixel_id = coords_to_index(x, y, viewport_size.x as usize);
+                let area = edge_function(sc0, sc1, sc2);
 
-        let area = edge_function(sc0, sc1, sc2);
-
-        if let Some(bary) = barycentric_coordinates(coords, sc0, sc1, sc2, area) {
-            let correction = bary.x * rec0 + bary.y * rec1 + bary.z * rec2;
-            let correction = 1.0 / correction;
-            let depth = bary.x * ndc0.z + bary.y * ndc1.z + bary.z * ndc2.z;
-            if depth < z_buffer[i] {
-                z_buffer[i] = depth;
-                let color = bary.x * v0.color + bary.y * v1.color + bary.z * v2.color;
-                let color = color * correction;
-                let mut color = to_argb8(
-                    255,
-                    (color.x * 255.0) as u8,
-                    (color.y * 255.0) as u8,
-                    (color.z * 255.0) as u8,
-                );
-                if let Some(tex) = texture {
-                    let tex_coords = bary.x * v0.uv + bary.y * v1.uv + bary.z * v2.uv;
-                    let tex_coords = tex_coords * correction;
-
-                    color = tex.argb_at_uv(tex_coords.x, tex_coords.y);
+                if let Some(bary) = barycentric_coordinates(coords, sc0, sc1, sc2, area) {
+                    let correction = bary.x * rec0 + bary.y * rec1 + bary.z * rec2;
+                    let correction = 1.0 / correction;
+                    let depth = bary.x * ndc0.z + bary.y * ndc1.z + bary.z * ndc2.z;
+                    if depth < z_buffer[pixel_id] {
+                        z_buffer[pixel_id] = depth;
+                        let color = bary.x * v0.color + bary.y * v1.color + bary.z * v2.color;
+                        let color = color * correction;
+                        let mut color = to_argb8(
+                            255,
+                            (color.x * 255.0) as u8,
+                            (color.y * 255.0) as u8,
+                            (color.z * 255.0) as u8,
+                        );
+                        if let Some(tex) = texture {
+                            let tex_coords = bary.x * v0.uv + bary.y * v1.uv + bary.z * v2.uv;
+                            let tex_coords = tex_coords * correction;
+                            color = tex.argb_at_uv(tex_coords.x, tex_coords.y);
+                        }
+                        buffer[pixel_id] = color;
+                    }
                 }
-
-                *pixel = color;
             }
         }
     }
@@ -123,5 +123,29 @@ pub fn raster_mesh(
     for triangle in mesh.triangles() {
         let vertices = mesh.get_vertices_from_triangle(*triangle);
         raster_triangle(&vertices, mvp, texture, buffer, z_buffer, viewport_size);
+    }
+}
+
+pub fn triangle_screen_bounding_box(
+    positions: &[Vec2; 3],
+    viewport_size: Vec2,
+) -> Option<BoundingBox2D> {
+    let bb = get_triangle_bounding_box_2d(positions);
+
+    if bb.left >= viewport_size.x || bb.right < 0.0 || bb.bottom >= viewport_size.y || bb.top < 0.0
+    {
+        None
+    } else {
+        let left = bb.left.max(0.0);
+        let right = bb.right.min(viewport_size.x - 1.0);
+        let bottom = bb.bottom.max(0.0);
+        let top = bb.top.min(viewport_size.y - 1.0);
+
+        Some(BoundingBox2D {
+            left,
+            right,
+            top,
+            bottom,
+        })
     }
 }
